bee08655b7eb7801ab15696266fac514ab733c77
Kamran Kiani <kiani.km@gmail.com>
2024-10-24 16:42:09 +0330

    Create main.js

---
+++ public/js/main.js
@@ -0,0 +1,66 @@
+const cards = document.querySelectorAll(".product__item");
+const btnOpen = document.querySelector(".js-nav-open");
+const btnClose = document.querySelector(".js-nav-close");
+const media = window.matchMedia("(width < 45em)");
+const topNavMenu = document.querySelector(".nav__menu");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+
+Array.prototype.forEach.call(cards, (card) => {
+  let down,
+    up,
+    link = card.querySelector("h3 a");
+  card.onmousedown = () => (down = +new Date());
+  card.onmouseup = () => {
+    up = +new Date();
+    if (up - down < 200) {
+      link.click();
+    }
+  };
+
+  card.style.cursor = "pointer";
+});
+
+function openMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "true");
+  topNavMenu.removeAttribute("inert");
+  topNavMenu.removeAttribute("style");
+  main.setAttribute("inert", "");
+  footer.setAttribute("inert", "");
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "false");
+  topNavMenu.setAttribute("inert", "");
+  main.removeAttribute("inert");
+  footer.removeAttribute("inert");
+  btnOpen.focus();
+
+  setTimeout(() => {
+    topNavMenu.style.transition = "none";
+  }, 500);
+}
+
+function setupTopNav(e) {
+  if (e.matches) {
+    // is mobile
+    console.log("is mobile");
+    topNavMenu.setAttribute("inert", "");
+    topNavMenu.style.transition = "none";
+  } else {
+    // is tablet/desktop
+    console.log("is desktop");
+    closeMobileMenu();
+    topNavMenu.removeAttribute("inert");
+  }
+}
+
+setupTopNav(media);
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+
+media.addEventListener("change", function (e) {
+  setupTopNav(e);
+});
